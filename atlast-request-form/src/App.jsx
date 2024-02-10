2930f9588ddb42658c48b2856454501514d491e9
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2024-02-10 15:11:05 -0500

    basic init

--- atlast-request-form/src/App.jsx
+++ atlast-request-form/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+    import './App.css';
+    
+    /*
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import Home from './Components/Home.jsx';
+    //import Dark from "./Assets/Darkmodelogo.png"
+    //import Light from "./Assets/Lightmodelogo.png"
+    //import UnderConstruction from './Components/UnderConstruction/UnderConstruction';
+    import Header from './Components/Header/Navbar.jsx'
+    import Resume from './Components/Resume/Resume';
+    import Contact from './Components/Contact/Contact';
+    import Bookshelf2 from './Components/Bookshelf/Bookshelf2';
+    import Photography from './Components/Photography/Photography';
+    import Music from './Components/Music/Music';
+    import About from './Components/About/About';
+    import Projects from './Components/Projects/ProjectHome';
+    import PageNotFound from './Components/Pagenotfound/Pagenotfound.jsx';
+    import Blog from './Components/Blog/Blog.jsx';
+    //import Footer from './Components/Footer/Footer';
+    //import Character from './components/Character';
+    //import Characterlistbypage from './components/Characterbypage';
+    //import CharacterHistory from './components/Characterhistory';
+    //import Comics from './components/Comics';
+   // import Comicsbypage from './components/Comicsbypage';
+    //import Stories from './components/Stories';
+    //import Storiesbypage from './components/Storiesbypage';
+    */
+    import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+
+import Form from './Components/Form/Form';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <Router>
+        {/*
+        <div >
+           
+            <header>
+             <Header/>
+            </header>
+            */}
+            <div >
+              <Routes>
+                <Route path='/' element={<Form />} />
 
-export default App
+              </Routes>
+            </div>
+            {/*
+            <footer>
+              
+            </footer>
+           
+          </div> */} 
+        </Router>
+        
+      );
+    };
+    
+    export default App;
